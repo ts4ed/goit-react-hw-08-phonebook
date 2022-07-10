f7860a485639021476dbcc061009b8ec228d018b
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-07-10 17:38:56 +0300

    update

--- src/components/AppBarComponents/UserMenu/UserMenu.jsx
+++ src/components/AppBarComponents/UserMenu/UserMenu.jsx
@@ -1,14 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useRef } from 'react';
-import { authSelectors, authOperations } from '../../../redux/auth';
+import { authOperations } from '../../../redux/auth';
 import LogoutIcon from '@mui/icons-material/Logout';
-import { UserContainer, Button, UserName, Text } from './UserMenu.styled';
+import { UserContainer, Button, Text } from './UserMenu.styled';
 
 export default function UserMenu() {
   const dispatch = useDispatch();
-  const name = useSelector(
-    authSelectors.getUsername ? authSelectors.getUsername : ''
-  );
+
   const screenWidth = window.screen.width;
   const isDesktop = useRef(true);
 
@@ -23,9 +21,6 @@ export default function UserMenu() {
   }
   return (
     <UserContainer>
-      <UserName style={{ color: '#fff' }}>
-        {isDesktop.current ? `Welcome, ${name}` : ''}
-      </UserName>
       <Button onClick={logoutCab} type="button">
         <Text>Logout</Text>
         <LogoutIcon />
